32ce40b1076e4fb210670cf0f3fcb89f4fed0179
Rich Wareham <rich@richwareham.com>
2020-06-07 14:36:24 +0100

    Updates

---
+++ precache-manifest.33e8966af5349f66f051956626bd23c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cba20c94839b94f34948e929a514f0",
+    "url": "/camcount/index.html"
+  },
+  {
+    "revision": "65ee6312736b678f9003",
+    "url": "/camcount/static/css/main.7f48018e.chunk.css"
+  },
+  {
+    "revision": "1a816a6488829bd84293",
+    "url": "/camcount/static/js/2.78a26439.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/camcount/static/js/2.78a26439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ee6312736b678f9003",
+    "url": "/camcount/static/js/main.9cabb325.chunk.js"
+  },
+  {
+    "revision": "fb426eae431fbdb7584f",
+    "url": "/camcount/static/js/runtime-main.b77a7335.js"
+  }
+]);
